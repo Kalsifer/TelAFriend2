2ceaec8e0c52de78dca0097602e41e790df2dbb2
Alana Morin <daddyslittlebyte@gmail.com>
2015-08-11 13:44:36 -0700

    Changed menu to scrollspy, starting to tweak images for mobile friendly

--- js/slicksettings.js
+++ js/slicksettings.js
@@ -30,4 +30,13 @@ $(document).ready(function(){
         autoplaySpeed: 2000
     });
 
+    $('.variable-width').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+
 });
